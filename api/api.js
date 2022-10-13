6ee153c8181b7f13615feaf508ae49ed46444bb7
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 15:23:38 +0200

    Create Artist Routes - Import and connect artistsRouter for path /artists in app.js

--- api/api.js
+++ api/api.js
@@ -2,3 +2,7 @@ const express = require('express');
 const apiRouter = express.Router();
 
 module.exports = apiRouter;
+
+//Import and connect artistsRouter for path '/artists'
+const artistsRouter = require('./artists');
+apiRouter.use('/artists', artistsRouter);
