2fc85e27d50709211c3b607268333a0f71175620
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 15:37:29 +0200

    Create Artist Routes - Create GET request to retrieve all api/artists or error

--- api/artists.js
+++ api/artists.js
@@ -5,3 +5,16 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 module.exports = artistsRouter;
+
+// GET /api/artists
+artistsRouter.get('/', (req, res, next) => {
+  db.all(
+    `SELECT * FROM Artist WHERE is_currently_employed = 1;`,
+    function (err, rows) {
+      if (err) {
+        next(err);
+      }
+      res.status(200).send({artists: rows});
+    }
+  );
+});
