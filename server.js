f582590caf87a028e551dc70d991fb41e05f0baf
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 12:13:42 +0200

    Setup server - Set morgan logging dev

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorHandler = require('errorhandler');
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(errorHandler());
+app.use(morgan('dev'));
