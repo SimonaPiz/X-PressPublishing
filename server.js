39afff5defbe295786d57028355bcbb05693b9ff
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 12:12:20 +0200

    Setup server - Set errorHandler

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const errorHandler = require('errorhandler');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(errorHandler());
