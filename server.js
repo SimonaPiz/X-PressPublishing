ed84008ff259aaa027052be93c22ee3688211ffa
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 12:16:20 +0200

    Setup server - Start server with port

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(errorHandler());
 app.use(morgan('dev'));
+
+app.listen(PORT, (req, res, next) => {
+  console.log('Server is running on port ' + PORT);
+})
