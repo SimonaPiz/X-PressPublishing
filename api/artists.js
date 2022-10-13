d3231d525c1428c248fc549617b40d17c147a888
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 15:48:33 +0200

    Create Artist Routes - Create GET param artistId to set it for all Router

--- api/artists.js
+++ api/artists.js
@@ -6,6 +6,24 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 module.exports = artistsRouter;
 
+// Add param ':artistId' to set it in all Router
+artistsRouter.param(':artistId', (req, res, next, index) => {
+  db.get(
+    `SELECT * FROM Artist WHERE id = ?;`,
+    [index],
+    function (err, row) {
+      if (err) {
+        next(err);
+      } else if (row) {
+        req.artist = row;
+        next();
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  );
+});
+
 // GET /api/artists
 artistsRouter.get('/', (req, res, next) => {
   db.all(
