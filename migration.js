e7a3fc2d5f80da6191d2254c908b6d287fd4a87b
Simona Pizio <simona.pizio@gmail.com>
2022-10-13 15:09:06 +0200

    Create Artist Table - Fix command to create table Artist to log error

--- migration.js
+++ migration.js
@@ -10,6 +10,11 @@ db.run(`CREATE TABLE IF NOT EXISTS Artist (
   biography TEXT NOT NULL,
   is_currently_employed INTEGER DEFAULT 1
 )`, function (err) {
-  console.log(err);
-  return;
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log('Database is correctly created');
+    return;
+  }
 });
