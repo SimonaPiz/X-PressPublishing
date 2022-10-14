8aafc1ddffa37c80085433df27613358462f0b88
Simona Pizio <simona.pizio@gmail.com>
2022-10-14 14:54:07 +0200

    Create Series Routes - Create GET request to retrieve all series

--- api/series.js
+++ api/series.js
@@ -5,3 +5,16 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 module.exports = seriesRouter;
+
+// GET /api/series
+seriesRouter.get('/', (req, res, next) => {
+  db.all(
+    `SELECT * FROM series;`,
+    function (err, rows) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).send({series: rows});
+    }
+  );
+});
