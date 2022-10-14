441f37bec826f305eba0967a4231ceb105bcac54
Simona Pizio <simona.pizio@gmail.com>
2022-10-14 14:48:42 +0200

    Create Series Routes - Create seriesRouter Express and connect it to api Router

--- api/api.js
+++ api/api.js
@@ -3,6 +3,10 @@ const apiRouter = express.Router();
 
 module.exports = apiRouter;
 
-//Import and connect artistsRouter for path '/artists'
+//Import and connect artistsRouter for path 'api/artists'
 const artistsRouter = require('./artists');
 apiRouter.use('/artists', artistsRouter);
+
+//Import and connect seriesRouter for path 'api/series'
+const seriesRouter = require('./series');
+apiRouter.use('/series', seriesRouter);
