a0bb29a22f01d0770b190d9010c640b159d15f0a
Simona Pizio <simona.pizio@gmail.com>
2022-10-15 14:17:10 +0200

    Create Issue Routes - Create GET request to retrieve all issues from seriesId

--- api/issues.js
+++ api/issues.js
@@ -5,3 +5,16 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 module.exports = issuesRouter;
+
+// GET /api/series/:seriesId/issues
+issuesRouter.get('/', (req, res, next) => {
+  db.all(
+    `SELECT * FROM Issue WHERE series_id = ${req.params.seriesId};`,
+    function (err, rows) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).send({issues: rows});
+    }
+  );
+});
