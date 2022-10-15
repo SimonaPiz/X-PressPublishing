af3ddce0c172ce895b0f661acdd65e9a654634d0
Simona Pizio <simona.pizio@gmail.com>
2022-10-15 14:35:20 +0200

    Create Issue Routes - Create POST request to create issue of series by seriesId

--- api/issues.js
+++ api/issues.js
@@ -18,3 +18,55 @@ issuesRouter.get('/', (req, res, next) => {
     }
   );
 });
+
+// Create function to validate new issue before adding to the database
+const validateData = (req, res, next) => {
+  const newData = req.body.issue;
+  //console.log(newData);
+  newData.issueNumber = Number(newData.issueNumber);
+  newData.artistId = Number(newData.artistId);
+
+  if (
+    typeof newData.name !== 'string' || !newData.name ||
+    newData.issueNumber < 0 || !newData.issueNumber ||
+    typeof newData.publicationDate !== 'string' || !newData.publicationDate ||
+    newData.artistId < 0 || !newData.artistId
+  ) {
+    res.sendStatus(400);
+    return;
+  } else {
+    //console.log('Data are valid');
+    next();
+  }
+};
+
+// POST - Create new issue
+issuesRouter.post('/', validateData, (req, res, next) => {
+  //console.log(req.body.issue);
+  const newIssue = req.body.issue;
+
+  db.run(
+    `INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id)
+    VALUES (
+      '${newIssue.name}',
+      ${newIssue.issueNumber},
+      '${newIssue.publicationDate}',
+      ${newIssue.artistId},
+      ${req.params.seriesId}
+    );`,
+    function (err) {
+      if (err) {
+        return next(err);
+      }
+      db.get(
+        `SELECT * FROM Issue WHERE id = ${this.lastID};`,
+        (err, row) => {
+          if (err) {
+            return next(err);
+          }
+          res.status(201).send({issue: row});
+        }
+      );
+    }
+  );
+});
