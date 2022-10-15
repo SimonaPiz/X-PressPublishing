4543281e01915028d6ea44633297805fa88b2360
Simona Pizio <simona.pizio@gmail.com>
2022-10-15 10:33:30 +0200

    Create Series Routes - Create GET param seriesId to set it for all Router

--- api/series.js
+++ api/series.js
@@ -6,6 +6,32 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 module.exports = seriesRouter;
 
+// Add param ':seriesId' to set it in all Router
+seriesRouter.param('seriesId', (req, res, next, index) => {
+  const seriesId = Number(index);
+  if (seriesId && seriesId >= 0) {  
+    db.get(
+      `SELECT * FROM series WHERE id = ?;`,
+      [seriesId],
+      function (err, row) {
+        if (err) {
+          return next(err);
+        } else if (row) {
+          req.series = row;
+          req.seriesId = seriesId;
+          next();
+        } else {
+          res.sendStatus(404);
+          return;
+        }
+      }
+    );
+  } else {
+    res.sendStatus(404);
+    return;
+  }
+});
+
 // GET /api/series
 seriesRouter.get('/', (req, res, next) => {
   db.all(
